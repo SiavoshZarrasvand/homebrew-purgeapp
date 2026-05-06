5a3b60c27fa9dc94800f6148be5d2580f07f1756
Siavosh Zarrasvand <siavosh@zarrasvand.com>
2026-05-07 01:38:42 +0300

    Release v3.0.8: update formula url, sha256, version

--- Formula/purgeapp.rb
+++ Formula/purgeapp.rb
@@ -1,10 +1,10 @@
 class Purgeapp < Formula
   desc "Completely remove a macOS app and all its leftover files"
   homepage "https://github.com/SiavoshZarrasvand/homebrew-purgeapp"
-  url "https://github.com/SiavoshZarrasvand/homebrew-purgeapp/archive/refs/tags/v3.0.7.tar.gz"
-  sha256 "a7ff821064df922e7c0190af1f03d3b6ac0f840708775dd165a992a8b328877b"
+  url "https://github.com/SiavoshZarrasvand/homebrew-purgeapp/archive/refs/tags/v3.0.8.tar.gz"
+  sha256 "26a40ce81ded1ab53f16a68524d4b835bc108b54f6521841a7360298c6363c13"
   license "MIT"
-  version "3.0.7"
+  version "3.0.8"
 
   depends_on :macos
 
